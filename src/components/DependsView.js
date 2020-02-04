feaa733717af56dead5d48197bc68307c4c868ad
Galacca <ivisql@gmail.com>
2020-02-04 19:17:33 +0200

    more clarity

--- src/components/DependsView.js
+++ src/components/DependsView.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import isLinkable from '../utils/linker';
-import formatPackageNames from '../utils/helpers';
 
 const DependsView = (props) => {
   return (
     <li>
 Depends:
       {props.dependsValue.map((dv, index) => {
-        const linkable = isLinkable(formatPackageNames(dv), props.packages);
+        const linkable = isLinkable(dv, props.packages);
         if (index + 1 !== props.dependsValue.length && !props.dependsValue[index + 1].startsWith('|')) {
           if (linkable) return <Link to={linkable} key={dv}>{dv.concat(', ')}</Link>;
           return dv.concat(', ');
--- src/utils/linker.js
+++ src/utils/linker.js
@@ -1,6 +1,9 @@
+import formatPackageNames from './helpers';
+
 const isLinkable = (dependancy, packages) => {
+  const formattedDeps = formatPackageNames(dependancy);
   const packageArray = Object.keys(packages);
-  if (packageArray.includes(dependancy)) return dependancy;
+  if (packageArray.includes(formattedDeps)) return formattedDeps;
   return false;
 };
 
